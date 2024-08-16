66f0e47ae834428421742ccc8730af444b1d2900
S3BAAS <sebastejeda392@gmail.com>
2024-08-16 00:36:21 -0300

    SSR test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import netlify from '@astrojs/netlify';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
-  adapter: netlify(),
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
